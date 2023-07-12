c7f033f8126bd6c47fa36bf7ac6f87dc2f94e1af
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2023-07-12 14:50:55 +0300

    feat: create DependencyResolvers

---
+++ EventManagementApplication.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,44 @@
+﻿using EventManagementApplication.Business.Abstract;
+using EventManagementApplication.Business.Concrete;
+using EventManagementApplication.DataAccess.Abstract;
+using EventManagementApplication.DataAccess.Concrete;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementApplication.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IEventService>().To<EventService>().InSingletonScope();
+            Bind<IEventRepository>().To<EventRepository>().InSingletonScope();
+
+            Bind<ILocationService>().To<LocationService>().InSingletonScope();
+            Bind<ILocationRepository>().To<LocationRepository>().InSingletonScope();
+
+            Bind<IEventService>().To<EventService>().InSingletonScope(); // Değiştirilecek
+            Bind<IInvitationRepository>().To<InvitationRepository>().InSingletonScope();
+
+            Bind<IEventService>().To<EventService>().InSingletonScope();// Değiştirilecek
+            Bind<INotificationRepository>().To<NotificationRepository>().InSingletonScope();
+
+            Bind<IEventService>().To<EventService>().InSingletonScope();// Değiştirilecek
+            Bind<IRoleRepository>().To<RoleRepository>().InSingletonScope();
+
+            Bind<IEventService>().To<EventService>().InSingletonScope();// Değiştirilecek
+            Bind<IUserDetailRepository>().To<UserDetailRepository>().InSingletonScope();
+
+            Bind<IEventService>().To<EventService>().InSingletonScope();// Değiştirilecek
+            Bind<IUserInvitationMappingRepository>().To<UserInvitationMappingRepository>().InSingletonScope();
+
+            Bind<IEventService>().To<EventService>().InSingletonScope();// Değiştirilecek
+            Bind<IUserRepository>().To<UserRepository>().InSingletonScope();
+
+        }
+    }
+}
