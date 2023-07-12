ddd4413e616adfdf9746512062eb07bdcd2db98f
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2023-07-12 13:00:48 +0300

    feat: created Location Service

---
+++ EventManagementApplication.Business/Concrete/LocationService.cs
@@ -0,0 +1,60 @@
+﻿using EventManagementApplication.Business.Abstract;
+using EventManagementApplication.Business.ValidationRules.FluentValidation;
+using EventManagementApplication.DataAccess.Abstract;
+using EventManagementApplication.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementApplication.Business.Concrete
+{
+    public class LocationService : ILocationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LocationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+       
+        public void Create(Location entity)
+        {
+
+            _unitOfWork.Locations.Add(entity);
+            _unitOfWork.Save();
+        }
+
+        
+        public void Delete(int id)
+        {
+            var location = _unitOfWork.Locations.GetById(id);
+            if (location != null)
+            {
+                _unitOfWork.Locations.Remove(location);
+                _unitOfWork.Save();
+            }
+        }
+
+       
+        public IEnumerable<Location> GetAll()
+        {
+            return _unitOfWork.Locations.GetAll();
+        }
+
+        public Location GetById(int id)
+        {
+            return _unitOfWork.Locations.GetById(id);
+        }
+
+       
+        public void Update(Location entity)
+        {
+
+            _unitOfWork.Locations.Update(entity);
+            _unitOfWork.Save();
+        }
+    }
+}
---
+++ EventManagementApplication.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementApplication.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTool
+    {
+        public static void FluentValidate(IValidator validator, object entity)
+        {
+            var validationContext = new ValidationContext<object>(entity);
+            var result = validator.Validate(validationContext);
+
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
