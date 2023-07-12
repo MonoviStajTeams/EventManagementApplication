7078a84b89165f4c684860936acddd43ac9f16c7
EcemKonca <85101485+EcemKonca@users.noreply.github.com>
2023-07-12 14:12:19 +0300

    RoleService and User Service created

---
+++ EventManagementApplication.Business/Concrete/UserService.cs
@@ -0,0 +1,54 @@
+﻿using EventManagementApplication.Business.Abstract;
+using EventManagementApplication.Business.ValidationRules.FluentValidation;
+using EventManagementApplication.DataAccess.Abstract;
+using EventManagementApplication.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementApplication.Business.Concrete
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Create(User user)
+        {
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.Save();
+        }
+
+        public void Delete(int id)
+        {
+            var user = _unitOfWork.Users.GetById(id);
+            if (user != null)
+            {
+                _unitOfWork.Users.Remove(user);
+                _unitOfWork.Save();
+            }
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _unitOfWork.Users.GetAll();
+        }
+
+        public User GetById(int id)
+        {
+            return _unitOfWork.Users.GetById(id);
+        }
+
+        public void Update(User entity)
+        {
+            _unitOfWork.Users.Update(entity);
+            _unitOfWork.Save();
+        }
+    }
+}
