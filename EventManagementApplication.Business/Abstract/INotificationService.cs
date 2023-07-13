a90f11b4afab903813589e6e6f4b4f0523114c06
Elif Özker <111710264+elifozker@users.noreply.github.com>
2023-07-13 10:58:19 +0300

    created NotificationService

--- EventManagementApplication.Business/Abstract/INotificationService.cs
+++ EventManagementApplication.Business/Abstract/INotificationService.cs
@@ -9,5 +9,10 @@ namespace EventManagementApplication.Business.Abstract
 {
     public interface INotificationService : IGenericService<Notification>
     {
+        void Create(Notification entity);
+        void Delete(int id);
+        IEnumerable<Notification> GetAll();
+        Notification GetById(int id);
+        void Update(Notification entity);
     }
 }
