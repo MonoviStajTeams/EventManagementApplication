8843114f7a795a96afe52796d8525cd5618cb964
Elif Özker <111710264+elifozker@users.noreply.github.com>
2023-07-14 17:32:44 +0300

    created UserDetails Test and InvitationTests

--- EventManagementApplication.Tests/InvitationTests.cs
+++ EventManagementApplication.Tests/InvitationTests.cs
@@ -1,11 +1,139 @@
+using EventManagementApplication.Business.Concrete;
+using EventManagementApplication.DataAccess.Abstract;
+using EventManagementApplication.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
 namespace EventManagementApplication.Tests
 {
     public class InvitationTests
     {
+        [ExpectedException(typeof(ValidationException))]
         [Fact]
-        public void Test1()
+        public void Create_Invitation_SuccessfullyCreated()
         {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IInvitationRepository invitationRepository = new Mock<IInvitationRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.Invitations).Returns(invitationRepository);
+
+            InvitationService invitationService = new InvitationService(mockUnitOfWork.Object);
+
+            Invitation invitationToCreate = new Invitation
+            {
+                Title = "Test Invitation",
+                Description = "Invitation Description",
+                UserId = 1,
+                User = new User(),
+                EventId = 1,
+                Event = new Event()
+            };
+
+            // Act
+            invitationService.Create(invitationToCreate);
+
+
+            mockUnitOfWork.Verify(uow => uow.Invitations.Add(invitationToCreate), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void Delete_ExistingInvitation_SuccessfullyDeleted()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IInvitationRepository invitationRepository = new Mock<IInvitationRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.Invitations).Returns(invitationRepository);
+
+            InvitationService invitationService = new InvitationService(mockUnitOfWork.Object);
+
+            int invitationIdToDelete = 1;
+
+            // Act
+            invitationService.Delete(invitationIdToDelete);
+
 
         }
+
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void Get_All_Invitations()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            Mock<IInvitationRepository> mockInvitationRepository = new Mock<IInvitationRepository>();
+
+            List<Invitation> expectedInvitations = new List<Invitation>()
+            {
+                new Invitation { Id = 1,Title = "Test Invitation",Description = "Invitation Description", User = new User(),UserId=1,Event = new Event(), EventId = 1 },
+                new Invitation { Id = 2,Title = "Test Invitation 2",Description = "Invitation Description 2", User = new User(),UserId=2,Event = new Event(), EventId = 2 },
+
+            };
+
+            mockUnitOfWork.Setup(uow => uow.Invitations.GetAll()).Returns(expectedInvitations);
+
+            InvitationService invitationService = new InvitationService(mockUnitOfWork.Object);
+
+            // Act
+            IEnumerable<Invitation> invitations = invitationService.GetAll();
+
+            // Assert
+            Xunit.Assert.Equal(expectedInvitations, invitations);
+        }
+
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void Update_Invitation_SuccessfullyUpdated()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IInvitationRepository invitationRepository = new Mock<IInvitationRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.Invitations).Returns(invitationRepository);
+
+            InvitationService invitationService = new InvitationService(mockUnitOfWork.Object);
+
+            Invitation invitationToUpdate = new Invitation
+            {
+                Id = 1,
+                Title = "Test Event 2",
+                Description = "Event Description 2",
+                UserId = 2,
+                User = new User()
+            };
+
+            // Act
+            invitationService.Update(invitationToUpdate);
+
+            // Assert or further test steps if needed
+            // Verify that the event is updated as expected
+            mockUnitOfWork.Verify(uow => uow.Invitations.Update(invitationToUpdate), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void GetById_Invitation()
+        {
+            // Arrange
+            int invitationId = 1;
+            Invitation expectedInvitation = new Invitation { Id = invitationId, Title = "Test Invitation", Description = "Invitation Description", User = new User(), UserId = 1, Event = new Event(), EventId = 1 };
+
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            Mock<IInvitationRepository> mockInvitationRepository = new Mock<IInvitationRepository>();
+
+            mockUnitOfWork.Setup(uow => uow.Invitations.GetById(invitationId)).Returns(expectedInvitation);
+
+            InvitationService invitationService = new InvitationService(mockUnitOfWork.Object);
+
+            // Act
+            Invitation invitation = invitationService.GetById(invitationId);
+
+            // Assert
+            Xunit.Assert.Equal(expectedInvitation, invitation);
+        }
+
+
     }
 }
--- EventManagementApplication.Tests/UserDetailTests.cs
+++ EventManagementApplication.Tests/UserDetailTests.cs
@@ -1,11 +1,136 @@
+using EventManagementApplication.Business.Concrete;
+using EventManagementApplication.DataAccess.Abstract;
+using EventManagementApplication.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 namespace EventManagementApplication.Tests
 {
     public class UserDetailTests
     {
+        [ExpectedException(typeof(ValidationException))]
         [Fact]
-        public void Test1()
+        public void Create_UserDetail_SuccessfullyCreated()
         {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IUserDetailRepository userDetailRepository = new Mock<IUserDetailRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.UserDetails).Returns(userDetailRepository);
+
+            UserDetailService userDetailService = new UserDetailService(mockUnitOfWork.Object);
+
+            UserDetail userDetailToCreate = new UserDetail
+            {
+
+                PhoneNumber = "123456789",
+                ProfileImagePath = "",
+                UserId = 1,
+                User = new User()
+            };
+
+            // Act
+            userDetailService.Create(userDetailToCreate);
+
+
+            mockUnitOfWork.Verify(uow => uow.UserDetails.Add(userDetailToCreate), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void Delete_ExistingId_SuccessfullyDeleted()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IUserDetailRepository userDetailRepository = new Mock<IUserDetailRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.UserDetails).Returns(userDetailRepository);
+
+            UserDetailService userDetailService = new UserDetailService(mockUnitOfWork.Object);
+
+            int userIdToDelete = 1;
+
+            // Act
+            userDetailService.Delete(userIdToDelete);
+
 
         }
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void GetAll_ReturnsAllUserDetails()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IUserDetailRepository userDetailRepository = new Mock<IUserDetailRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.UserDetails).Returns(userDetailRepository);
+
+            UserDetailService userDetailService = new UserDetailService(mockUnitOfWork.Object);
+
+            // Act
+            var result = userDetailService.GetAll();
+
+            Xunit.Assert.NotNull(result);
+        }
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void GetById_ExistingUserDetail_ReturnsUserDetail()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IUserDetailRepository userDetailRepository = new Mock<IUserDetailRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.UserDetails).Returns(userDetailRepository);
+
+            UserDetailService userDetailService = new UserDetailService(mockUnitOfWork.Object);
+
+            int userDetailId = 1;
+            UserDetail expectedUserDetail = new UserDetail
+            {
+                Id = userDetailId,
+                PhoneNumber = "123456789",
+                ProfileImagePath = "",
+                UserId = 1,
+                User = new User()
+            };
+
+      
+            mockUnitOfWork.Setup(uow => uow.UserDetails.GetById(userDetailId)).Returns(expectedUserDetail);
+
+            // Act
+            var result = userDetailService.GetById(userDetailId);
+
+            // Assert
+            Xunit.Assert.NotNull(result);
+            Xunit.Assert.Equal(expectedUserDetail.Id, result.Id);
+            Xunit.Assert.Equal(expectedUserDetail.PhoneNumber, result.PhoneNumber);
+            Xunit.Assert.Equal(expectedUserDetail.ProfileImagePath, result.ProfileImagePath);
+            
+        }
+        [ExpectedException(typeof(ValidationException))]
+        [Fact]
+        public void Update_UserDetail_SuccessfullyUpdated()
+        {
+            // Arrange
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            IUserDetailRepository userDetailRepository = new Mock<IUserDetailRepository>().Object;
+            mockUnitOfWork.Setup(uow => uow.UserDetails).Returns(userDetailRepository);
+
+            UserDetailService userDetailService = new UserDetailService(mockUnitOfWork.Object);
+
+            UserDetail userDetailToUpdate = new UserDetail
+            {
+                Id = 1,
+                PhoneNumber = "234567891",
+                ProfileImagePath = "",
+                UserId = 1,
+                User = new User()
+            };
+
+            // Act
+            userDetailService.Update(userDetailToUpdate);
+
+            // Assert or further test steps if needed
+            // Verify that the event is updated as expected
+            mockUnitOfWork.Verify(uow => uow.UserDetails.Update(userDetailToUpdate), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
     }
 }
