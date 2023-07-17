0f147e66378bbba4b361563776a4148a236ec7a4
ECEM <ecemkonca@hotmail.com>
2023-07-17 14:00:21 +0300

    FinishedEventList ViewComponent created

---
+++ EventManagementApplication.WebUI/ViewComponents/FinishedEventList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementApplication.WebUI.ViewComponents
+{
+    public class FinishedEventList : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
