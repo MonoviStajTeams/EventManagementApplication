bd3884bd657934de0502696b05e0b640485b63cf
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2023-07-21 15:04:34 +0300

    feat: created UserInvitation

---
+++ EventManagementApplication.Entities/dtos/AddUserInvitationMappingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementApplication.Entities.dtos
+{
+    public class AddUserInvitationMappingModel
+    {
+        public int[] UserIds { get; set; }
+        public int InvitationId { get; set; }
+    }
+}
