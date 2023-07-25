cf49c1ba5ad3ff0faec8afa97fc8bad949af9cb1
ECEM <ecemkonca@hotmail.com>
2023-07-25 19:31:31 +0300

    UserDashboard added

---
+++ EventManagementApplication.MAUI/UserDashboard.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementApplication.MAUI
+{
+    public partial class UserDashboard: ContentPage
+    {
+        public UserDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
