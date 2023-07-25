5a68a865229b4fc5837abb2d126093e50495d7e8
ECEM <ecemkonca@hotmail.com>
2023-07-25 13:05:30 +0300

    AddEvent & UpdateEvent created

--- EventManagementApplication.MAUI/MainPage.xaml.cs
+++ EventManagementApplication.MAUI/MainPage.xaml.cs
@@ -2,13 +2,15 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
+        private async void SignUp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddEvent());
+        }
 
-     
     }
 }
