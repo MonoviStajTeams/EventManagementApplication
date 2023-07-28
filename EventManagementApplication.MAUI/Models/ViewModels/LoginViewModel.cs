1513ec935eaa4f8a933b9d9f6d8450845c9f5e19
ECEM <ecemkonca@hotmail.com>
2023-07-28 15:52:14 +0300

    Login View Model added

--- EventManagementApplication.MAUI/Models/ViewModels/LoginViewModel.cs
+++ EventManagementApplication.MAUI/Models/ViewModels/LoginViewModel.cs
@@ -1,15 +1,28 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using EventManagementApplication.Entities.Concrete;
+using EventManagementApplication.MAUI.Models.ApiModels;
+using EventManagementApplication.MAUI.Services.Abstract;
+using EventManagementApplication.MAUI.Services.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace EventManagementApplication.MAUI.ViewModels
 {
     public partial class LoginViewModel : ObservableObject
     {
+        private readonly IAuthApiService _authApiService;
+
+        public LoginViewModel()
+        {
+            _authApiService = new AuthApiService("user");
+        }
+
         [ObservableProperty]
         private string email;
 
@@ -22,5 +35,17 @@ namespace EventManagementApplication.MAUI.ViewModels
         {
 
         }
+        [RelayCommand]
+        private async Task LoginUser()
+        {
+            var entity = new LoginApiResponse
+            {
+                Email = email,
+                Password = password
+            };
+
+            await _authApiService.Login(entity);
+        }
+
     }
 }
