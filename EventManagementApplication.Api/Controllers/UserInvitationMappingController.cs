82003b2ef2bfbb255bd89ba334c5cc6a5f75c9b4
GOD <GOD@DESKTOP-1UQONKI>
2023-07-30 21:43:52 +0300

    UserInvitationMappingController ekledim (incelenecek)

---
+++ EventManagementApplication.Api/Controllers/UserInvitationMappingController.cs
@@ -0,0 +1,64 @@
+﻿using EventManagementApplication.Business.Abstract;
+using EventManagementApplication.Business.Concrete;
+using EventManagementApplication.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementApplication.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInvitationMappingController : ControllerBase
+    {
+        private readonly IUserInvitationMappingService _userİnvitationMappingService;
+        
+        public UserInvitationMappingController(IUserInvitationMappingService userInvitationMappingService)
+        {
+            _userİnvitationMappingService = userInvitationMappingService;   
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var userInvitationMappingServiceGetById = _userİnvitationMappingService.GetById(id);
+            return Ok(userInvitationMappingServiceGetById);
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            var userInvitationMappingServiceGetAll = _userİnvitationMappingService.GetAll();
+            return Ok(userInvitationMappingServiceGetAll);
+        }
+        [HttpPost]
+        [Route("CreateMapping")]
+        public IActionResult CreateMapping(UserInvitationMapping userInvitationMapping)
+        {
+            _userİnvitationMappingService.Create(userInvitationMapping);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("UpdateMapping")]
+        public IActionResult UpdateMapping(UserInvitationMapping userInvitationMapping)
+        {
+            _userİnvitationMappingService.Update(userInvitationMapping);
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("DeleteMapping")]
+        public IActionResult DeleteMapping(int id)
+        {
+            _userİnvitationMappingService.Delete(id);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("AcceptEvent")]
+        public IActionResult AcceptEvent(int id)
+        { 
+            _userİnvitationMappingService.AcceptEvent(id);
+            return Ok();
+            //reddetme için bir çözüm gerekiyor
+        }
+    }
+}
