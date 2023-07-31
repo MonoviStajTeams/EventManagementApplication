7510c20c698e0bcc15f5fa2cf8a8df03f9cad251
Atabey Demir <atadem26@hotmail.com>
2023-07-31 09:29:15 +0300

    MyEvent page created

---
+++ EventManagementApplication.MAUI/Pages/MyEvent.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace YourNamespace
+{
+    public partial class MyEvent : ContentPage
+    {
+
+        private MyEventViewModel viewModel;
+
+        public MyEvent()
+        {
+            InitializeComponent();
+
+            
+            viewModel = new MyEventViewModel();
+            BindingContext = viewModel;
+        }
+
+        
+        private void OnEventClicked(object sender, EventArgs e)
+        {
+          
+
+        }
+    }
+}
+
