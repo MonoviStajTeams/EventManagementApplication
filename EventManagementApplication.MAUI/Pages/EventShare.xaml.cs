cd0d1e9c79d109908d48c5421258793831a506b6
GOD <GOD@DESKTOP-1UQONKI>
2023-08-29 09:02:32 +0300

    eventshare view

---
+++ EventManagementApplication.MAUI/Pages/EventShare.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace EventManagementApplication.MAUI.Pages
+{
+    public partial class EventShare : ContentPage
+    {
+        public EventShare()
+        {
+            InitializeComponent();
+        }
+    }
+}
